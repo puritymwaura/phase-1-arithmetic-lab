43aa359f2d5d3678a3ab3d6ca4fefc4e32e5a916
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-06-07 01:41:55 -0400

    commit tree 41777b5e0bb9c7a6c5fa8245301e4559d20093cc

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() *100)
+let num3 = 49;
+let num4 = 5
+let mod = num3 % num4;
+let max = Math.max(2,5,7,20)
+console.log(multiply);
+console.log(random);
+console.log(mod);
